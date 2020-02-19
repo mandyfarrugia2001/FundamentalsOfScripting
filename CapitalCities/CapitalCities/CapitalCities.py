24853ae119f337f24dc02aaa99030077b72ce16e
mauren9401 <56023961+mauren9401@users.noreply.github.com>
2020-02-19 14:04:42 +0100

    Created a text file containing list of countries together with their capital city
    Created a text file and populated an array with countries and capital cities delimited by a dash. There is a function that populates the array/list and returns said list. This array will be used in a function which creates the text file and copies each element in the list inside the text file created, hence the use of the for loop was mandatory. In the main function, we called the function which creates the text files.

--- CapitalCities/CapitalCities/CapitalCities.py
+++ CapitalCities/CapitalCities/CapitalCities.py
@@ -1,1 +1,33 @@
+def returnListCountries():
+    countriesCities = ["Albania-Tirana", "Andorra-Andorra la Vella",
+                       "Armenia-Yerevan", "Austria-Vienna",
+                       "Azerbaijan-Baku", "Belarus-Minsk",
+                       "Belgium-Brussels", "Bosnia and Herzegovina-Sarajevo",
+                       "Bulgaria-Sofia", "Croatia-Zagreb",
+                       "Cyprus-Nicosia", "Czech Republic-Prague",
+                       "Denmark-Copenhagen", "Estonia-Tallinn",
+                       "Finland-Helsinki", "France-Paris",
+                       "Georgia-Tbilisi", "Germany-Berlin",
+                       "Greece-Athens", "Hungary-Budapest",
+                       "Iceland-Reykjavik", "Ireland-Dublin",
+                       "Italy-Rome", "Kazakhstan-Astana",
+                       "Kosovo-Pristina", "Latvia-Riga",
+                       "Liechtenstein-Vaduz", "Lithuania-Vilnius",
+                       "Luxembourg-Luxembourg city", "Macedonia (FYROM)-Skopje",
+                       "Malta-Valletta", "Moldova-Chisinau", "Monaco-Monaco city",
+                       "Montenegro-Podgorica", "Netherlands-Amsterdam",
+                       "Norway-Oslo", "Poland-Warsaw", "Portugal-Lisbon",
+                       "Romania-Bucharest", "Russia-Moscow", 
+                       "San Marino-San Marino city", "Serbia-Belgrade",
+                       "Slovakia-Bratislava", "Spain-Madrid",
+                       "Sweden-Stockholm", "Switzerland-Bern",
+                       "Turkey-Ankara", "Ukraine-Kyiv", 
+                       "United Kingdom-London"]
+
+    return countriesCities
+
+countriesTextFile = createFile("countries.txt")
+
+
+
 
